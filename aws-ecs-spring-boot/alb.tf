c290c351b8a73c5944ea9777a366298a60a486be
Julia <juliakurylo18@gmail.com>
2021-12-14 01:16:38 +0200

    renamed resources

--- aws-ecs-spring-boot/alb.tf
+++ aws-ecs-spring-boot/alb.tf
@@ -1,13 +1,13 @@
 # alb.tf
 
 resource "aws_alb" "main" {
-  name            = "myapp-load-balancer"
+  name            = "myapp-load-balancer-cloud-tech"
   subnets         = aws_subnet.public.*.id
   security_groups = [aws_security_group.lb.id]
 }
 
 resource "aws_alb_target_group" "app" {
-  name        = "myapp-target-group"
+  name        = "myapp-target-group_cloud_tech"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
--- aws-ecs-spring-boot/logs.tf
+++ aws-ecs-spring-boot/logs.tf
@@ -1,8 +1,8 @@
 # logs.tf
 
 # Set up CloudWatch group and log stream and retain logs for 30 days
-resource "aws_cloudwatch_log_group" "myapp_log_group" {
-  name              = "/ecs/myapp"
+resource "aws_cloudwatch_log_group" "myapp_log_group_cloud_tech" {
+  name              = "/ecs/myappcloudtech"
   retention_in_days = 30
 
   tags = {
@@ -12,5 +12,5 @@ resource "aws_cloudwatch_log_group" "myapp_log_group" {
 
 resource "aws_cloudwatch_log_stream" "myapp_log_stream" {
   name           = "my-log-stream"
-  log_group_name = aws_cloudwatch_log_group.myapp_log_group.name
+  log_group_name = aws_cloudwatch_log_group.myapp_log_group_cloud_tech.name
 }
