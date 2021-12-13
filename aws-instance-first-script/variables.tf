14c6c2cbdeef630c7321f03d79abdbfc7588904b
Julia <juliakurylo18@gmail.com>
2021-12-13 22:24:45 +0200

    change config to US

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
-  default = "eu-west-2"
+	default = "us-east-1"
 }
 
 variable "ami_id" {
   type = "map"
 
   default = {
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
+    us-east-1    = "ami-035b3c7efe6d061d5"
   }
 }
