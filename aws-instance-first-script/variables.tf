3e803182757ca53cf854f2e317e51be035ed49c0
Julia <juliakurylo18@gmail.com>
2021-12-13 22:20:20 +0200

    change to eu-west-2

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
-  default = "eu-central-1"
+  default = "eu-west-2"
 }
 
 variable "ami_id" {
   type = "map"
 
   default = {
-    eu-central-1 = "ami-0a7559a0ef82639f2"
+    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
   }
 }
